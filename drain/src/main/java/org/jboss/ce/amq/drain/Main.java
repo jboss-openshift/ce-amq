7c4f8d11641187d50f4a89839f047f516f24aeaf
Ales Justin <ales.justin@gmail.com>
2016-04-25 16:28:10 +0200

    Initial CE-AMQ skeleton.

---
+++ drain/src/main/java/org/jboss/ce/amq/drain/Main.java
@@ -0,0 +1,10 @@
+package org.jboss.ce.amq.drain;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
