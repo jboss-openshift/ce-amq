64e954d116c3021d1de1a450155d489ff4f2f131
Ales Justin <ales.justin@gmail.com>
2016-05-12 22:55:00 +0200

    Set lib env var.

--- drain/drain.sh
+++ drain/drain.sh
@@ -2,4 +2,7 @@
 
 echo ${AMQ_HOME}
 
-java -cp ${AMQ_HOME}/lib/activemq-client-5.11.0.jar:${AMQ_HOME}/lib/slf4j-api-1.7.10.jar:${AMQ_HOME}/lib/geronimo-jms_1.1_spec-1.1.1.jar:${AMQ_HOME}/lib/hawtbuf-1.11.jar:${AMQ_HOME}/lib/geronimo-j2ee-management_1.1_spec-1.0.1.jar:${AMQ_HOME}/lib/jcl-over-slf4j-1.7.10.jar -jar ${AMQ_HOME}/lib/optional/ce-amq-drain-1.0.0-SNAPSHOT.jar
+export AMQ_LIB=${AMQ_HOME}/lib
+echo ${AMQ_LIB}
+
+java -cp ${AMQ_LIB}/activemq-client-5.11.0.jar:${AMQ_LIB}/slf4j-api-1.7.10.jar:${AMQ_LIB}/geronimo-jms_1.1_spec-1.1.1.jar:${AMQ_LIB}/hawtbuf-1.11.jar:${AMQ_LIB}/geronimo-j2ee-management_1.1_spec-1.0.1.jar:${AMQ_LIB}/jcl-over-slf4j-1.7.10.jar -jar ${AMQ_LIB}/optional/ce-amq-drain-1.0.0-SNAPSHOT.jar
